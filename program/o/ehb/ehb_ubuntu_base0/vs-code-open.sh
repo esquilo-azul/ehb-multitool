d8311a7550b69ddd9ad02680b79c33f0dfe021b4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-05-16 13:23:55 -0300

    Program[o/ehb/ehb_ubuntu_base0/vs-code-open]: move de "cliutils"#"e/ehbrs/ehb_ubuntu_base0/vs-code-open".

---
+++ program/o/ehb/ehb_ubuntu_base0/vs-code-open.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source "${BASH_TO_REQUIRE}"
+
+PROJECT_EXTENSION='.code-workspace'
+
+function files_list() {
+  find "${DROPBOX_ROOT}/devel/vscode-workspaces" -name "*${PROJECT_EXTENSION}" | sort
+}
+
+hash_init OPTIONS
+while read -r FILE_PATH; do
+  var_set_by FILE_BASENAME basename "${FILE_PATH}" "${PROJECT_EXTENSION}"
+  hash_put OPTIONS "${FILE_BASENAME}" "${FILE_PATH}"
+done < <(files_list)
+
+ehb_open_selector 'OPTIONS' "$@"
