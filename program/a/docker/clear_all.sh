29f692fca7a2ef35ae83604b41a6db75ff74e49c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-08-11 21:44:56 -0300

    Program[a/docker/clear_all]: refatora com `docker system prune`.

--- program/a/docker/clear_all.sh
+++ program/a/docker/clear_all.sh
@@ -2,41 +2,4 @@
 
 source "${BASH_TO_REQUIRE}"
 
-function run_on_run() {
-  FIRST_CMD=()
-  while true; do
-    ARG="$1"
-    shift
-    if [ "$ARG" == ';' ]; then
-      break
-    else
-      FIRST_CMD+=("$ARG")
-      infov 'After' "${FIRST_CMD[@]}"
-    fi
-  done
-  if [ -n "$(docker "$@")" ]; then
-    docker "${FIRST_CMD[@]}" $(docker "$@")
-  fi
-}
-
-function stop_containers() {
-  run_on_run kill \; ps -q
-}
-
-function remove_containers() {
-  run_on_run rm \; ps -aq
-}
-
-function remove_volumes() {
-  run_on_run volume rm \; volume ls -q
-}
-
-function remove_images() {
-  run_on_run rmi -f \; images -q
-}
-
-function remove_networks() {
-  run_on_run network rm \; network ls -q
-}
-
-cliutils_run_jobs stop_containers remove_containers remove_volumes remove_images remove_networks
+docker system prune --all --volumes --force
