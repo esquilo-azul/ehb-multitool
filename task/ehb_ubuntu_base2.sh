82aff8975f52e789174e5a7f183070e025cb15fb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-09-08 17:13:17 -0300

    Task[ehb_ubuntu_base2].

---
+++ task/ehb_ubuntu_base2.sh
@@ -0,0 +1,12 @@
+function task_dependencies() {
+  outout_nl \
+    ehb_ubuntu_base2/debian_packages \
+    ehb_ubuntu_base2/firefox \
+    ehb_ubuntu_base2/syncthing/service \
+    ehb_ubuntu_base2/telegram \
+    ehb_ubuntu_base2/pcloud
+}
+
+function task_condition() {
+  return
+}
