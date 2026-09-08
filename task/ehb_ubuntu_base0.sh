52ce725166e6b84026d0ee38e0446730db973ced
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-09-08 17:13:09 -0300

    Task[ehb_ubuntu_base0].

---
+++ task/ehb_ubuntu_base0.sh
@@ -0,0 +1,8 @@
+function task_dependencies() {
+  outout_nl ehb_ubuntu_base1 ehb_ubuntu_base0/debian_packages ehb_ubuntu_base0/printing \
+    ehb_ubuntu_base0/snap_packages
+}
+
+function task_condition() {
+  return
+}
