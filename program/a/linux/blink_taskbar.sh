bf2db72319c1c5e326b9abf362eba29f131fbd7a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-09-10 22:40:40 -0300

    Program[a/linux/blink_taskbar]: lista janelas disponíveis.

--- program/a/linux/blink_taskbar.sh
+++ program/a/linux/blink_taskbar.sh
@@ -2,13 +2,16 @@
 
 source "${BASH_TO_REQUIRE}"
 
-WIN="$1"
+WIN="${1:-}"
 INTERVAL="${2:-0.6}"
 DURATION="${3:-10}"
 
 if [ -z "$WIN" ]; then
-  echo "Uso: $0 '<título da janela>|<id da janela>' [intervalo_s] [duração_s]" >&2
-  exit 1
+  echo "Uso: $0 '<título da janela>|<id da janela>' [intervalo_s] [duração_s]"
+  echo
+  echo "Janelas disponíveis (id  título):"
+  wmctrl -l | awk '{ id=$1; $1=$2=$3=""; sub(/^ +/, ""); printf "  %s  %s\n", id, $0 }'
+  exit 0
 fi
 
 WMCTRL_ID_OPT=()
