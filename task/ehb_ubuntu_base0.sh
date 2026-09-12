8d7f8bd058b0fd2f285255a2da24024d7ad12d9d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-09-12 14:28:46 -0300

    Task[ehb_ubuntu_base0]: requer "eac_ce/debian_packages".

--- task/ehb_ubuntu_base0.sh
+++ task/ehb_ubuntu_base0.sh
@@ -1,6 +1,6 @@
 function task_dependencies() {
-  outout_nl ehb_ubuntu_base1 ehb_ubuntu_base0/debian_packages ehb_ubuntu_base0/printing \
-    ehb_ubuntu_base0/snap_packages
+  outout_nl ehb_ubuntu_base1 eac_ce/debian_packages ehb_ubuntu_base0/debian_packages \
+    ehb_ubuntu_base0/printing ehb_ubuntu_base0/snap_packages
 }
 
 function task_condition() {
